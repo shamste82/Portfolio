0e646ddd5b9e0394c295ad34f7f8a6a40d81e319
Steffen HB <steffenhb@live.no>
2016-11-19 21:40:22 +0100

    finished with mobile

--- app/js/app.js
+++ app/js/app.js
@@ -7,24 +7,35 @@ $(document).ready(function() {
         $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
         $('.portfolio-image').addClass('animated bounceOutRight');
         $('.welcome-arrow-down').css('visibility', 'hidden');
-        $('.page2').css('visibility', 'hidden');
+        $('#welcome-heading').css('visibility', 'hidden');
     });
-    //$('.portfolio-image').hide();
-
 
     $('.dropdown').on('hide.bs.dropdown', function (e) {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
         $('.portfolio-image').removeClass('animated bounceOutRight');
         $('.portfolio-image').addClass('animated bounceInRight');
-        $('.page2').css('visibility', 'visible');
+        $('#welcome-heading').css('visibility', 'visible');
 
     });
 
-    $('.portfolio-image').hover(function () {
-        $('.welcome-arrow-down').css('visibility', 'visible');
-        $('.welcome-arrow-down').addClass('animated flash');
+    // The arrow blinks when page2 is touched/hovered
+    $('.page2').hover(function () {
+        $('.div-arrow-right-img').css('visibility', 'visible');
+        $('.div-arrow-right-img').addClass('animated flash');
         window.setTimeout(function () {
-            $('.welcome-arrow-down').removeClass('animated flash');
+            $('.div-arrow-right-img').css('visibility', "hidden");
+            $('.div-arrow-right-img').removeClass('animated flash');
+        }, 2000);
+
+    });
+
+    // The arrow blinks when page3 is touched/hovered
+    $('.page3').hover(function () {
+        $('.arrow-right-mob').css('visibility', 'visible');
+        $('.arrow-right-mob').addClass('animated flash');
+        window.setTimeout(function () {
+            $('.arrow-right-mob').css('visibility', "hidden");
+            $('.arrow-right-mob').removeClass('animated flash');
         }, 2000);
 
     });
@@ -34,6 +45,13 @@ $(document).ready(function() {
 
     // Calls the fullpage plugin
     $('#fullpage').fullpage({
-
+        anchors:['section-welcome', 'section-about', 'section-projects', 'section-contact'],
+        controlArrows: false,
+        paddingTop: '0px',
     });
+
+
+
 });
+
+
